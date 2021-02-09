574d3121a747f918e05f94c249876bfebb82d7a6
alex6744 <alexngai6744@gmail.com>
2021-02-09 02:27:25 -0800

    added feature of createBtn

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -4,14 +4,16 @@
     var $removeBtn, $editBtn, $createBtn;
     var $tablebody;
     var userService = new AdminUserServiceClient();
-    var users=[{username: 'CS5610', password:20, firstname:'Spring',lastname:'Spring',role:'Spring'}];
+    var users=[];
 
 
     function createUser(user) {
         userService.createUser(user)
             .then(function (actualUser){
+                console.log(actualUser)
                 users.push(actualUser)
-                renderUsers(users)
+                console.log(users)
+                renderUsers(actualUser)
             })
         console.log("sdsds")
 
@@ -40,8 +42,10 @@
         $tablebody.empty()
         for(var i=0;i<users.length;i++){
             var user=users[i]
+            //console.log(user)
             $tablebody.prepend(`
                     <tr>
+                        
                         <td>${user.username}</td>
                         <td>${user.password}</td>
                         <td>${user.firstname}</td>
@@ -66,21 +70,27 @@
     function main() {
         $usernameFld=$(".usernameFld")
         $passwordFld=$(".passwordFld")
-        $firstNameFld=$(".firstnameFld")
+        $firstNameFld=$(".firstNameFld")
         $lastNameFld=$(".lastNameFld")
         $roleFld=$(".roleFld")
       //  $removeBtn=jQuery("#wbdv-create")
         //$editBtn=jQuery("#wbdv-create")
         $createBtn=jQuery(".wbdv-create")
         $tablebody=jQuery("tbody")
-        $createBtn.click(()=>
-            createUser({username:$usernameFld.val(),
-                             password: $passwordFld.val(),
-                             firstname: $firstNameFld.val(),
-                             lastname: $lastNameFld.val(),
-                              role: $roleFld.val()
-            })
-
+        $createBtn.click(()=> {
+                createUser({
+                    username: $usernameFld.val(),
+                    password: $passwordFld.val(),
+                    firstname: $firstNameFld.val(),
+                    lastname: $lastNameFld.val(),
+                    role: $roleFld.val()
+                })
+                $usernameFld.val("")
+                $passwordFld.val("")
+                $firstNameFld.val("")
+                $lastNameFld.val("")
+                $roleFld.val("")
+            }
         )
         userService.findAllUsers()
             .then(function (actualUsersFromServer){
