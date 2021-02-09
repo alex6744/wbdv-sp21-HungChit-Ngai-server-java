d03d1d9509ecb7abd9078e0a9f520a7f2513ec66
alex6744 <alexngai6744@gmail.com>
2021-02-08 21:00:15 -0800

    created user model

--- src/main/java/com/example/wbdvsp21HungChitNgaiserverjava/models/User.java
+++ src/main/java/com/example/wbdvsp21HungChitNgaiserverjava/models/User.java
@@ -1,4 +1,71 @@
 package com.example.wbdvsp21HungChitNgaiserverjava.models;
 
 public class User {
+    private long ID;
+    private String username;
+    private String password;
+    private String firstname;
+    private String  lastname;
+    private String role;
+    public User(long ID, String username, String password, String firstname, String lastname, String role) {
+        this.ID = ID;
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
 }
