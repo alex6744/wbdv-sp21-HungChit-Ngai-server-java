4fcb5e5f35af8e29ad80f408575e2c354644894a
alex6744 <alexngai6744@gmail.com>
2021-02-08 21:00:36 -0800

    created user controller

---
+++ src/main/java/com/example/wbdvsp21HungChitNgaiserverjava/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.wbdvsp21HungChitNgaiserverjava.controller;
+
+import com.example.wbdvsp21HungChitNgaiserverjava.models.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+public class UserController {
+    static List<User> users=new ArrayList<>();
+    static {
+        users.add(new User(123,"alex1","3123","alex","ngai","student"));
+        users.add(new User(456,"al231","31efsdf3","ex","ng","student"));
+
+    }
+//    // POST - Creating
+//    @PostMapping("/users")
+//    public List<User> createUser(@RequestBody User user) {
+//        users.add(user);
+//        return users;
+//    }
+
+    // GET - Reading
+    @GetMapping("/api/001501828/users")
+    public List<User> findAllUsers() {
+        return users;
+    }
+//
+//    @GetMapping("/api/001501828/users/od")
+//    public List<User> findUserByID(String ID) {
+//        return users;
+//    }
+//    @PutMapping("/api/001501828/users/id")
+//    public void updateUser(User user,String ID) {
+//        //return users;
+//    }
+//    @DeleteMapping("/api/001501828/users/id")
+//    public void deleteUser(User user) {
+//        users.remove(user);
+//
+//    }
+
+}
