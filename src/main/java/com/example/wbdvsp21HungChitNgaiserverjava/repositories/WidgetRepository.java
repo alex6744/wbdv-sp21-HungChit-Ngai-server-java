474f84456a789d5e719a8dc9e714ce6f08ccdd9d
alex6744 <alexngai6744@gmail.com>
2021-03-30 16:32:35 -0700

    add repositories

---
+++ src/main/java/com/example/wbdvsp21HungChitNgaiserverjava/repositories/WidgetRepository.java
@@ -0,0 +1,9 @@
+package com.example.wbdvsp21HungChitNgaiserverjava.repositories;
+
+import com.example.wbdvsp21HungChitNgaiserverjava.models.Widget;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface WidgetRepository extends CrudRepository<Widget, String> {
+
+}
